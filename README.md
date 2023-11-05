4f0de6ec98a774b85c302662dda94b7f8f86fbb6
patinohoyos <santiagopatino@quadi.co>
2023-11-05 15:42:40 -0500

    Added information to the readme.md counter

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Frontend-test
 Repositorio para el alojamiento de la prueba de Frontend
+
+## Uso
+
+Cómo jugar
+
+Adivina la palabra oculta en cinco intentos.
+
+Cada intento debe ser una palabra válida de 5 letras.
+
+Después de cada intento el color de las letras cambia para mostrar qué tan cerca estás de acertar la palabra.
+
+Puede haber letras repetidas. Las pistas son independientes para cada letra.
+
+¡Una palabra nueva cada 5 minutos!
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// create the word dictionary
 const dictionary = ['bulla',
     'neuma',
     'tasen',
@@ -4809,3 +4810,29 @@ function startup() {
 
 startup();
 
+// format time
+
+function formatTime(minutes, seconds){
+    return  `${minutes} : ${seconds}`;
+}
+
+// countdown
+
+function countdown(totalMinutes){
+    let seconds = totalMinutes * 60;
+    const countdownElement = document.getElementById('countdown');
+    const countdownInterval = setInterval(() => {
+        if (seconds > 0 ) {
+            const minutes = Math.floor(seconds / 60);
+            const remainingSeconds = seconds % 60;
+            countdownElement.innerHTML = formatTime(minutes, remainingSeconds);
+            seconds--;
+        } else {
+            clearInterval(countdownInterval);
+            countdownElement.innerHTML = "Cambio de palabra";
+        }
+        
+    }, 1000);
+}
+
+countdown(5);
